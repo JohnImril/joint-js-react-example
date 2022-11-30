9492044eec4907b6c3b9528717f50e1bc690c5a1
Никита Максимов <nmaksimov@logrocon.com>
2022-11-30 08:52:46 +0300

    interactive

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ import Graph from "./components/Graph/Graph";
 const App = () => {
   return (
     <div className="app">
-      <header className="app-header">Hello</header>
-      <h1>Class Component</h1>
+      <header className="app-header"></header>
+      <h1>Non-editable circuit</h1>
       <Graph />
 
-      <h1>Functional Component</h1>
+      <h1>Editable circuit</h1>
       <JointLayer />
     </div>
   );
